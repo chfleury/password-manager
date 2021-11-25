2a4b9d58f51e635fb925f081ce5e1697603c21b0
Christian Fleury <chfleurysiq@gmail.com>
2021-11-25 07:49:20 -0300

    feat: add all the fields on add_pass_dialog

--- lib/widgets/add_pass_dialog.dart
+++ lib/widgets/add_pass_dialog.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 
 import '../models/pass.dart';
 import '../constants.dart';
@@ -10,52 +9,64 @@ class AddPassDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     String _password;
     String _label;
+    String _login;
     final _formKey = GlobalKey<FormState>();
     return Dialog(
-      child: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(Constants.of(context).addPass),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Label'),
-              onSaved: (input) => _label = input,
-            ),
-            TextFormField(
-              validator: (input) => input.length < 3
-                  ? 'Password must contain at least 3 characters'
-                  : null,
-              onSaved: (input) => _password = input,
-              decoration: InputDecoration(labelText: 'Password'),
-            ),
-            ElevatedButton(
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    // Save the form
-                    _formKey.currentState.save();
+      child: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(Constants.of(context).addPass),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Label *'),
+                onSaved: (input) => _label = input,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Username (optional)'),
+                onSaved: (input) => _login = input,
+              ),
+              TextFormField(
+                validator: (input) => input.length < 3
+                    ? 'Password must contain at least 3 characters'
+                    : null,
+                onSaved: (input) => _password = input,
+                decoration: InputDecoration(labelText: 'Password *'),
+              ),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: TextButton(
+                      onPressed: () {}, child: Text("Pick an Icon"))),
+              ElevatedButton(
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      // Save the form
+                      _formKey.currentState.save();
 
-                    // Add new pass to DB
-                    await RepositoryService.addPass(Pass(
-                      isProtected: false,
-                      icon: 1,
-                      hasLogin: false,
-                      label: _label,
-                      password: _password,
-                    ));
+                      // Add new pass to DB
+                      await RepositoryService.addPass(Pass(
+                        isProtected: false,
+                        icon: 1,
+                        hasLogin: false,
+                        label: _label,
+                        password: _password,
+                      ));
 
-                    // Then pop dialog with new password to add in _passList
-                    Navigator.of(context).pop(Pass(
-                      isProtected: false,
-                      icon: 1,
-                      hasLogin: false,
-                      label: _label,
-                      password: _password,
-                    ));
-                  }
-                },
-                child: Text(Constants.of(context).done)),
-          ],
+                      // Then pop dialog with new password to add in _passList
+                      Navigator.of(context).pop(Pass(
+                        isProtected: false,
+                        icon: 1,
+                        hasLogin: false,
+                        label: _label,
+                        password: _password,
+                      ));
+                    }
+                  },
+                  child: Text(Constants.of(context).done)),
+            ],
+          ),
         ),
       ),
     );
